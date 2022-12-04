8666042d2ab4cb182033df4f7b9fe87e250e0778
lifay9092 <422980644@qq.com>
2022-12-04 10:55:15 +0800

    优化递归删除内存溢出

--- src/main/kotlin/cn/lifay/lerverhc/db/BaseDao.kt
+++ src/main/kotlin/cn/lifay/lerverhc/db/BaseDao.kt
@@ -1,9 +1,11 @@
 package cn.lifay.lerverhc.db
 
+import cn.lifay.lerverhc.model.HttpTool
 import cn.lifay.lerverhc.model.HttpTools
 import cn.lifay.lerverhc.model.HttpTools.httpTools
 import org.ktorm.dsl.delete
 import org.ktorm.dsl.eq
+import org.ktorm.dsl.inList
 import org.ktorm.entity.filter
 import org.ktorm.entity.toList
 
@@ -16,13 +18,39 @@ object BaseDao {
     }
 
     fun deleteDeepHttpToolById(id: String) {
-        val c = DbInfor.database.httpTools.filter { it.parentId eq id }.toList()
+        val c = getChildrenById(id)
         if (c.isNotEmpty()) {
-            c.forEach { deleteDeepHttpToolById(it.id) }
+            //缓存所有父ID
+            val temps = ArrayList<String>()
+            temps.add(id)
+            for (i in c) {
+                addDeepPanId(temps, i)
+            }
+            DbInfor.database.delete(HttpTools) { httpTool ->
+                httpTool.parentId inList temps
+            }
         }
         DbInfor.database.delete(HttpTools) { httpTool ->
             httpTool.id eq id
         }
+    }
+
+    private fun addDeepPanId(temps: ArrayList<String>, httpTool: HttpTool) {
+        val c = getChildrenById(httpTool.id)
+        if (c.isNotEmpty()) {
+            temps.add(httpTool.id)
+            for (i in c) {
+                addDeepPanId(temps, i)
+            }
+        }
+    }
+
+    fun getChildrenById(id: String): List<HttpTool> {
+        val c = DbInfor.database.httpTools.filter { it.parentId eq id }.toList()
+        if (c.isNotEmpty()) {
+            return c
+        }
+        return ArrayList()
     }
 
 }
